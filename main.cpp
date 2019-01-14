3057a39da37247e27da122c0e2dd5ef552b98bf4
Matthew Schupack <matthew.schupack@aexp.com>
2019-01-14 09:31:19 -0500

    formatting

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include "ray.h"
 
-vec3 color(const ray& r){
+vec3 color(const ray &r)
+{
 	vec3 unit_direction = unit_vector(r.direction());
-	float t= 0.5 * (unit_direction.y() + 1.0);
-	return (1-t) * vec3(0.0, 0.0,0.0) + t * vec3(0.5, 0.0, 0.0);
-
+	float t = 0.5 * (unit_direction.y() + 1.0);
+	return (1 - t) * vec3(0.0, 0.0, 0.0) + t * vec3(0.5, 0.0, 0.0);
 }
 
-int main(){
+int main()
+{
 	int nx = 2000;
 	int ny = 1000;
 
-	std::cout << "P3\n" << nx <<  " " << ny << "\n255\n";
+	std::cout << "P3\n"
+						<< nx << " " << ny << "\n255\n";
 	vec3 lower_left(-2.0, -1.0, -1.0);
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0,0.0,0.0);
+	vec3 origin(0.0, 0.0, 0.0);
 
-	for(int j=ny-1; j >=0; j--){
-		for(int i = 0; i < nx; i++){
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
-			ray r(origin, lower_left+u*horizontal+v*vertical);
+			ray r(origin, lower_left + u * horizontal + v * vertical);
 			vec3 col = color(r);
-			int ir = int(col.r() *255.99);
-			int ig = int(col.g() *255.99);
-			int ib = int(col.b() *255.99);
+			int ir = int(col.r() * 255.99);
+			int ig = int(col.g() * 255.99);
+			int ib = int(col.b() * 255.99);
 			std::cout << ir << " " << ig << " " << ib << std::endl;
 		}
 	}
--- ray.h
+++ ray.h
@@ -2,20 +2,21 @@
 #define RAYH
 #include "vec3.h"
 
-class ray{
-	public:
-		vec3 A;
-		vec3 B;
+class ray
+{
+public:
+	vec3 A;
+	vec3 B;
 
-		ray(){}
-		ray(const vec3& a, const vec3& b){
-			A= a;
-			B=b;
-		}
-		vec3 origin() const {return A;}
-		vec3 direction()const {return B;}
-		vec3 point_at_param(float t) const {return A + t*B;}
-
+	ray() {}
+	ray(const vec3 &a, const vec3 &b)
+	{
+		A = a;
+		B = b;
+	}
+	vec3 origin() const { return A; }
+	vec3 direction() const { return B; }
+	vec3 point_at_param(float t) const { return A + t * B; }
 };
 
 #endif
