e3072a471d72fd3737a05a19ab23a848fac57d19
Matthew Schupack <schu34@gmail.com>
2019-01-13 20:29:52 -0500

    gitignore

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include "ray.h"
+
+vec3 color(const ray& r){
+	vec3 unit_direction = unit_vector(r.direction());
+	float t= 0.5 * (unit_direction.y() + 1.0);
+	return (1-t) * vec3(0.0, 0.0,0.0) + t * vec3(0.5, 0.0, 0.0);
+
+}
 
 int main(){
-	int nx = 200;
-	int ny = 100;
+	int nx = 2000;
+	int ny = 1000;
 
 	std::cout << "P3\n" << nx <<  " " << ny << "\n255\n";
+	vec3 lower_left(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0,0.0,0.0);
 
 	for(int j=ny-1; j >=0; j--){
 		for(int i = 0; i < nx; i++){
-			float r = float(i) /float(nx);
-			float g = float(j)/float (ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left+u*horizontal+v*vertical);
+			vec3 col = color(r);
+			int ir = int(col.r() *255.99);
+			int ig = int(col.g() *255.99);
+			int ib = int(col.b() *255.99);
 			std::cout << ir << " " << ig << " " << ib << std::endl;
 		}
 	}
