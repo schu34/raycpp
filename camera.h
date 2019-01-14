443ec2e45cc992e58b2df678309c5f5f30e6ea60
Matthew Schupack <matthew.schupack@aexp.com>
2019-01-14 17:14:03 -0500

    add camera, hittable, hittable_list, sphere. implement anti-aliasing"

---
+++ camera.h
@@ -0,0 +1,28 @@
+#ifndef CAMERAH
+#define CAMERAH
+
+#include "ray.h"
+
+class camera
+{
+public:
+  camera()
+  {
+    lower_left = vec3(-2.0, -1.0, -1.0);
+    horizontal = vec3(4.0, 0.0, 0.0);
+    vertical = vec3(0.0, 2.0, 0.0);
+    origin = vec3(0.0, 0.0, 0.0);
+  }
+
+  ray get_ray(float u, float v)
+  {
+    return ray(origin, lower_left + u * horizontal + v * vertical - origin);
+  }
+
+  vec3 lower_left;
+  vec3 horizontal;
+  vec3 vertical;
+  vec3 origin;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,37 +1,55 @@
 #include <iostream>
-#include "ray.h"
+#include "sphere.h"
+#include "hittable_list.h"
+#include "camera.h"
+#include <random>
 
-vec3 color(const ray &r)
+vec3 color(const ray &r, hitable *world)
 {
-	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5 * (unit_direction.y() + 1.0);
-	return (1 - t) * vec3(0.0, 0.0, 0.0) + t * vec3(0.5, 0.0, 0.0);
+  hit_record rec;
+  if (world->hit(r, 0.0, MAXFLOAT, rec))
+  {
+    return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+  }
+  else
+  {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+  }
 }
 
 int main()
 {
-	int nx = 2000;
-	int ny = 1000;
+  int nx = 2000;
+  int ny = 1000;
+  int ns = 1000;
 
-	std::cout << "P3\n"
-						<< nx << " " << ny << "\n255\n";
-	vec3 lower_left(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-	for (int j = ny - 1; j >= 0; j--)
-	{
-		for (int i = 0; i < nx; i++)
-		{
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			ray r(origin, lower_left + u * horizontal + v * vertical);
-			vec3 col = color(r);
-			int ir = int(col.r() * 255.99);
-			int ig = int(col.g() * 255.99);
-			int ib = int(col.b() * 255.99);
-			std::cout << ir << " " << ig << " " << ib << std::endl;
-		}
-	}
+  hitable *list[2];
+  list[0] = new sphere(vec3(0, 0, -1), 0.5);
+  list[1] = new sphere(vec3(0, -100.5, -1), 100);
+
+  hitable *world = new hitable_list(list, 2);
+  camera cam;
+
+  for (int j = ny - 1; j >= 0; j--)
+  {
+    for (int i = 0; i < nx; i++)
+    {
+      vec3 col(0,0,0);
+      for(int s=0; s< ns; s++){
+        float u = float(i + drand48()) / float(nx);
+        float v = float(j + drand48()) / float(ny);
+        ray r = cam.get_ray(u, v);
+        col += color(r, world);
+      }
+      col /= ns;
+      int ir = int(col.r() * 255.99);
+      int ig = int(col.g() * 255.99);
+      int ib = int(col.b() * 255.99);
+      std::cout << ir << " " << ig << " " << ib << std::endl;
+    }
+  }
 }
