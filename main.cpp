272b3acfe056b9aff1f1857e23cd7d73ccce16d5
Matthew Schupack <matthew.schupack@aexp.com>
2019-01-18 16:39:27 -0500

    shadows n stuff

--- main.cpp
+++ main.cpp
@@ -3,13 +3,23 @@
 #include "hittable_list.h"
 #include "camera.h"
 #include <random>
+vec3 random_in_unit_sphere()
+{
+  vec3 p;
+  do
+  {
+    p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1);
+  return p;
+}
 
 vec3 color(const ray &r, hitable *world)
 {
   hit_record rec;
-  if (world->hit(r, 0.0, MAXFLOAT, rec))
+  if (world->hit(r, 0.01, MAXFLOAT, rec))
   {
-    return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target - rec.p), world);
   }
   else
   {
@@ -21,11 +31,12 @@ vec3 color(const ray &r, hitable *world)
 
 int main()
 {
-  int nx = 2000;
-  int ny = 1000;
-  int ns = 1000;
+  int nx = 400;
+  int ny = 200;
+  int ns = 100;
 
-  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  std::cout << "P3\n"
+            << nx << " " << ny << "\n255\n";
 
   hitable *list[2];
   list[0] = new sphere(vec3(0, 0, -1), 0.5);
@@ -38,14 +49,16 @@ int main()
   {
     for (int i = 0; i < nx; i++)
     {
-      vec3 col(0,0,0);
-      for(int s=0; s< ns; s++){
+      vec3 col(0, 0, 0);
+      for (int s = 0; s < ns; s++)
+      {
         float u = float(i + drand48()) / float(nx);
         float v = float(j + drand48()) / float(ny);
         ray r = cam.get_ray(u, v);
         col += color(r, world);
       }
       col /= ns;
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = int(col.r() * 255.99);
       int ig = int(col.g() * 255.99);
       int ib = int(col.b() * 255.99);
