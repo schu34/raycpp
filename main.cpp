96840efccf4f8795b9d7b1479c2af0304831af79
Matthew Schupack <matthew.schupack@aexp.com>
2019-01-24 17:43:32 -0500

    broken material implementation

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
+#include <random>
+#include "camera.h"
 #include "sphere.h"
 #include "hittable_list.h"
-#include "camera.h"
-#include <random>
-vec3 random_in_unit_sphere()
-{
-  vec3 p;
-  do
-  {
-    p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
-  } while (p.squared_length() >= 1);
-  return p;
-}
+#include "material.h"
 
-vec3 color(const ray &r, hitable *world)
+vec3 color(const ray &r, hitable *world, int depth)
 {
   hit_record rec;
   if (world->hit(r, 0.01, MAXFLOAT, rec))
   {
-    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-    return 0.5 * color(ray(rec.p, target - rec.p), world);
+    ray scattered;
+    vec3 attenuation;
+    if(depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)){
+      return color(scattered, world, depth+1);
+    } else{
+      return vec3(0,0,0);
+    }
   }
   else
   {
@@ -31,18 +28,20 @@ vec3 color(const ray &r, hitable *world)
 
 int main()
 {
-  int nx = 400;
-  int ny = 200;
+  int nx = 200;
+  int ny = 100;
   int ns = 100;
 
   std::cout << "P3\n"
             << nx << " " << ny << "\n255\n";
 
-  hitable *list[2];
-  list[0] = new sphere(vec3(0, 0, -1), 0.5);
-  list[1] = new sphere(vec3(0, -100.5, -1), 100);
+  hitable *list[4];
+  list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+  list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.3, 0.0)));
+  list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+  list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
 
-  hitable *world = new hitable_list(list, 2);
+  hitable *world = new hitable_list(list, 4);
   camera cam;
 
   for (int j = ny - 1; j >= 0; j--)
@@ -55,7 +54,7 @@ int main()
         float u = float(i + drand48()) / float(nx);
         float v = float(j + drand48()) / float(ny);
         ray r = cam.get_ray(u, v);
-        col += color(r, world);
+        col += color(r, world, 0);
       }
       col /= ns;
       col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
--- sphere.h
+++ sphere.h
@@ -2,14 +2,16 @@
 #define SPHEREH
 
 #include "hittable.h"
+#include "material.h"
 
 class sphere: public hitable{
   public:
   sphere(){}
-  sphere(vec3 cen, float r) : center(cen), radius(r){};
+  sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat(m) {};
   virtual bool hit (const ray &r, float t_min, float t_max, hit_record &rec) const;
   vec3 center;
   float radius;
+  material *mat;
 };
 
 bool sphere::hit(const ray &r, float t_min, float t_max, hit_record &rec) const{
