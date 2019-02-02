6a7359e67e55f05ffa0cae62a49b9e728588e223
Matthew Schupack <schu34@gmail.com>
2019-02-02 13:53:57 -0500

    add material utils

---
+++ material_util.h
@@ -0,0 +1,29 @@
+#include "ray.h"
+vec3 reflect(const vec3 &v, const vec3 &n)
+{
+  return v - 2 * dot(v, n) * n;
+}
+
+vec3 random_in_unit_sphere()
+{
+  vec3 p;
+  do
+  {
+    p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1);
+  return p;
+}
+
+bool refract(const vec3 &v, vec3 &n, float ni_over_nt, vec3 &refracted)
+{
+  vec3 uv = unit_vector(v);
+  float dt = dot(uv, n);
+  float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
+  if (discriminant > 0)
+  {
+    refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+    return true;
+  }
+  else
+    return false;
+}
